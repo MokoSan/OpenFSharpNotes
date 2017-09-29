d7c1a1303e8f3fc1f7a3deb0a40a9dcab3099e61
MokoSan <mukund.sharma92@gmail.com>
2017-09-29 15:38:32 -0700

    Commited 7 years of F#

---
+++ 7YearsOfF#/7YearsOfF#.fs
@@ -0,0 +1,12 @@
+module SevenYearsOfFSharp
+
+(*
+    1. www.github.com/Rickasaurus
+    2. www.richardminerich.com
+    3. http://github.com/BayardRock
+*)
+
+[<EntryPoint>]
+let main argv =
+    printfn "%A" argv
+    0 // return an integer exit code
