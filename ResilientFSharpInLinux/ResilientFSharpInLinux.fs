9880136afc9a32c5f663d5c34818453a293e6302
MokoSan <mukund.sharma92@gmail.com>
2017-09-29 13:14:10 -0700

    Final commit for the Resilient FSharp in Linux

--- ResilientFSharpInLinux/ResilientFSharpInLinux.fs
+++ ResilientFSharpInLinux/ResilientFSharpInLinux.fs
@@ -1,7 +1,7 @@
 module ResilientFSharpInLinux
 
 (*
-    1. Resilient: Able o withstand or recover quickly from difficult conditions
+    1. Resilient: Able to withstand or recover quickly from difficult conditions especially fucked up processes
     2. Unlikely events happen all the time.
     3. systemd - keeps processes running by PID
         1. c-groups: constraints on resources
@@ -10,8 +10,13 @@ module ResilientFSharpInLinux
     4. jounalid - structured logging daemon
     5. docker - standarized immutable infrastructure containers
     6. kubernetes - distributed scheduler
+        1. Robust for docker containers 
+        2. 3 instances of jobs for backups
     7. Need to latch on the Shutdown handler to clean up on application exit to use systemd 
     8. Suave used as a Restful api to check the status of the processes 
+    9. minikube - bootstraps a kubernetes environment written in Go
+    10. Include healthcheck APIs - http if possible
+    11. Aware of target Runtime differences
 *)
 
 [<EntryPoint>]
