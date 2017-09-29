df615b7e3a49795abc4367e8d893694dae8f8e4e
MokoSan <mukund.sharma92@gmail.com>
2017-09-29 12:19:25 -0700

    Added the Stock Cars Talk

---
+++ StockCarsAndMachineLearning/StockCarsAndMachineLearning.fs
@@ -0,0 +1,11 @@
+module StockCarsAndMachineLearning
+
+(*
+    1. Understand all the built in functions for Collections in F#
+    2. Measure, Measure and Measure -- infer data from the observations. 
+*)
+
+[<EntryPoint>]
+let main argv =
+    printfn "%A" argv
+    0 // return an integer exit code
