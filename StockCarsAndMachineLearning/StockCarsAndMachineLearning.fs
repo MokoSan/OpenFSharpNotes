5dff427b3462fb1fb7555400c0bdbfce2ca52100
MokoSan <mukund.sharma92@gmail.com>
2017-09-29 12:21:31 -0700

    Added the link of the github project

--- StockCarsAndMachineLearning/StockCarsAndMachineLearning.fs
+++ StockCarsAndMachineLearning/StockCarsAndMachineLearning.fs
@@ -3,6 +3,7 @@ module StockCarsAndMachineLearning
 (*
     1. Understand all the built in functions for Collections in F#
     2. Measure, Measure and Measure -- infer data from the observations. 
+    3. Code: https://github.com/jamessdixon/openfsharp
 *)
 
 [<EntryPoint>]
