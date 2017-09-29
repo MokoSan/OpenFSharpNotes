be864f283b72f97e69a0992a287282349fa4ec03
MokoSan <mukund.sharma92@gmail.com>
2017-09-29 06:08:45 -0700

    Added the Happy Stack

---
+++ HappyStack/HappyStack.fs
@@ -0,0 +1,17 @@
+module HappyStack
+
+(*
+    1. Full Stack F#
+        1. Client and Server 
+        2. Sharing code between client and server 
+    2. Suave
+        1. Webserver as a function
+    3. Fable = F# |> Babel
+    4. Elmish
+        1. Model View Update
+*)
+
+[<EntryPoint>]
+let main argv =
+    printfn "%A" argv
+    0 // return an integer exit code
